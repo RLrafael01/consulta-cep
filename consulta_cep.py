5c424b0923ad3692c13c9efb6094ad7de0ae8b24
Enzo <202507187732@alunos.ibmec.edu.br>
2026-08-27 16:53:02 -0300

    Exibe o endereco de forma legivel

--- consulta_cep.py
+++ consulta_cep.py
@@ -8,4 +8,12 @@ resposta = requests.get(url)
 
 dados = resposta.json()
 
-print(dados)
+print("CEP:", dados["cep"])
+
+print("Rua:", dados["logradouro"])
+
+print("Bairro:", dados["bairro"])
+
+print("Cidade:", dados["localidade"])
+
+print("Estado:", dados["uf"])
